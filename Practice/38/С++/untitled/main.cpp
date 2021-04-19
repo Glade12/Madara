4a7ff08b08507e613dc009027beeb66982bc0f73
Glade12 <serga4727@mail.ru>
2021-04-19 20:40:03 +0300

    36,37,38

---
+++ Practice/38/С++/untitled/main.cpp
@@ -0,0 +1,451 @@
+#include <iostream>
+#include <cmath>
+class Rational
+{
+private:
+    double numer, denom;
+public:
+    Rational()
+    {
+        this->numer = 0;
+        this->denom = 0 ;
+    }
+    Rational(int a, int b)
+    {
+        this->numer = a;
+        this->denom = b;
+    }
+    //RoFL __declspec(dllexport) static _Ret_valid_ int __stdcall gcd(_In_ _Satisfies_impl_(__Curr__ < INT32_MAX & __Curr__ > INT32_MIN) int a, _In_ int b)
+    static int gcd(int a, int b)
+    {
+        if (a < 0)
+        {
+            a = -a;
+        }
+        if (b < 0)
+        {
+            b = -b;
+        }
+        if (a == b)
+        {
+            return a;
+        }
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return b;
+            }
+        }
+        else if (b == 0)
+        {
+            if (a == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return a;
+            }
+        }
+        else if (a > b)
+        {
+            long tmp = a;
+            a = b;
+            b = tmp;
+        }
+        return gcd(a, b - a);
+    }
+    bool operator== (Rational x)
+    {
+        if (this->denom == 0 && x.numer == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            if ((this->numer / this->denom) == (x.numer / x.denom))
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    char is_inf()
+    {
+        if (this->denom != 0)
+        {
+            return 0;
+        }
+        if (this->numer == 0)
+        {
+            return 0;
+        }
+        return this->numer > 0 ? 1 : -1;
+    }
+    Rational operator+(Rational x)
+    {
+        if ((this->denom == 0 && this->numer == 0) || (x.denom == 0 && x.numer==0))
+        {
+            return Rational(0, 0);
+        }
+        if (this->is_inf() || x.is_inf())
+        {
+            if (this->is_inf() && x.is_inf())
+            {
+                auto sign = this->is_inf() + x.is_inf();
+                return (sign == 0 && !(this->numer + x.numer)) ? Rational(0, 0) : Rational(sign * 1, 0);
+            }
+            return this->is_inf() ? *this : x;
+        }
+        return Rational(this->numer * x.denom + this->denom * x.numer, this->denom * x.denom);
+    }
+    Rational operator-(Rational x)
+    {
+        if ((this->denom == 0 && this->numer == 0) || (x.denom == 0 && x.numer == 0))
+        {
+            return Rational(0, 0);
+        }
+        if (this->is_inf() || x.is_inf())
+        {
+            if (this->is_inf() && x.is_inf())
+            {
+                auto sign = this->is_inf() + -x.is_inf();
+                return (sign == 0 && !(this->numer + x.numer)) ? Rational(0, 0) : Rational(sign * 1, 0);
+            }
+            return this->is_inf() ? *this : Rational(-x.numer,0);
+        }
+        return Rational(this->numer * x.denom - this->denom * x.numer, this->denom * x.denom);
+    }
+    Rational operator*(Rational x)
+    {
+        return Rational(this->numer * x.numer, this->denom * x.denom);
+    }
+    Rational operator/(Rational x)
+    {
+        if ((this->denom == 0 && this->numer == 0) || (x.denom == 0 && x.numer == 0))
+        {
+            return Rational(0, 0);
+        }
+        if (this->is_inf() || x.is_inf())
+        {
+            if (this->is_inf() && x.is_inf())
+            {
+                return Rational(0, 0);
+            }
+            return this->is_inf() ?
+                ((double(x) >= 0 && (double(*this) >= 0)
+                    || (double(x) < 0 && (double(*this) < 0))))
+                ? Rational(1, 0) : Rational(-1, 0) : Rational(0, 1);
+        }
+        return Rational(this->numer * x.denom, this->denom * x.numer);
+    }
+    operator double()
+    {
+        return this->numer / this->denom;
+    }
+    operator bool()
+    {
+        if (this->denom != 0 && this->numer == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    int numerator()
+    {
+        return this->numer;
+    }
+    int denominator()
+    {
+        return this->denom;
+    }
+    bool isNaN()
+    {
+        return (this->numer == 0 && this->denom==0) ? 1 : 0;
+    }
+};
+
+bool equal(double a, double b, double e = 1E-10);
+
+
+int main()
+{
+    if (Rational::gcd(91, 65) == 13 &&
+        Rational::gcd(10, 3) == 1 &&
+        Rational::gcd(-10, 3) == 1 &&
+        Rational::gcd(10, -3) == 1 &&
+        Rational::gcd(-10, -3) == 1 &&
+        Rational::gcd(30, 10) == 10 &&
+        Rational::gcd(10, 30) == 10 &&
+        Rational::gcd(0, 10) == 10 &&
+        Rational::gcd(10, 0) == 10) std::cout << "gcd test passed\n";
+    else std::cout << "gcd test failed\n";
+
+    if (!Rational(22, 0).isNaN() &&
+        !Rational(22, 9).isNaN() &&
+        !Rational(0, 9).isNaN() &&
+        !Rational(-22, 9).isNaN() &&
+        !Rational(-22, 0).isNaN() &&
+        Rational(0, 0).isNaN()
+        ) std::cout << "isNaN test passed\n";
+    else std::cout << "isNaN test failed\n";
+
+    if (Rational(22, 0) == Rational(22, 0) &&
+        Rational(22, 0) == Rational(9, 0) &&
+        !(Rational(22, 0) == Rational(22, 9)) &&
+        !(Rational(22, 0) == Rational(-22, 9)) &&
+        !(Rational(22, 0) == Rational(-22, 0)) &&
+        !(Rational(22, 0) == Rational(0, 9)) &&
+        !(Rational(22, 0) == Rational(0, 0)) &&
+
+        Rational(22, 9) == Rational(22, 9) &&
+        Rational(22, 9) == Rational(-22, -9) &&
+        Rational(22, 9) == Rational(110, 45) &&
+        Rational(22, 9) == Rational(-110, -45) &&
+        !(Rational(22, 9) == Rational(-22, 9)) &&
+        !(Rational(22, 9) == Rational(22, -9)) &&
+        !(Rational(22, 9) == Rational(9, 22)) &&
+        !(Rational(22, 9) == Rational(22, 0)) &&
+        !(Rational(22, 9) == Rational(-22, 0)) &&
+        !(Rational(22, 9) == Rational(0, 9)) &&
+        !(Rational(22, 9) == Rational(0, 0)) &&
+
+        Rational(0, 1) == Rational(0, 1) &&
+        Rational(0, 1) == Rational(0, 9)  &&
+        Rational(0, 1) == Rational(0, -9)  &&
+        !(Rational(0, 1) == Rational(22, 9))  &&
+        !(Rational(0, 1) == Rational(-22, 9))  &&
+        !(Rational(0, 1) == Rational(22, 0)) &&
+        !(Rational(0, 1) == Rational(-22, 0)) &&
+        !(Rational(0, 1) == Rational(0, 0)) &&
+
+        Rational(-22, 9) == Rational(-22, 9) &&
+        Rational(-22, 9) == Rational(22, -9) &&
+        Rational(-22, 9) == Rational(-110, 45) &&
+        Rational(-22, 9) == Rational(110, -45) &&
+        !(Rational(-22, 9) == Rational(-22, -9)) &&
+        !(Rational(-22, 9) == Rational(22, 9)) &&
+        !(Rational(-22, 9) == Rational(9, -22)) &&
+        !(Rational(-22, 9) == Rational(22, 0)) &&
+        !(Rational(-22, 9) == Rational(-22, 0)) &&
+        !(Rational(-22, 9) == Rational(0, 9)) &&
+        !(Rational(-22, 9) == Rational(0, 0)) &&
+
+        Rational(-22, 0) == Rational(-22, 0) &&
+        Rational(-22, 0) == Rational(-9, 0) &&
+        !(Rational(-22, 0) == Rational(22, 9)) &&
+        !(Rational(-22, 0) == Rational(-22, 9)) &&
+        !(Rational(-22, 0) == Rational(22, 0)) &&
+        !(Rational(-22, 0) == Rational(0, 9)) &&
+        !(Rational(-22, 0) == Rational(0, 0)) &&
+
+        !(Rational(0, 0) == Rational(0, 0))
+        ) std::cout << "Equality test passed\n";
+    else std::cout << "Equality test failed\n";
+
+    if (Rational(22, 0) + Rational(22, 0) == Rational(22, 0) &&
+        Rational(22, 9) + Rational(22, 0) == Rational(22, 0) &&
+        Rational(0, 9) + Rational(22, 0) == Rational(22, 0) &&
+        Rational(-22, 9) + Rational(22, 0) == Rational(22, 0) &&
+        (Rational(-22, 0) + Rational(22, 0)).isNaN() &&
+
+        Rational(22, 0) + Rational(22, 9) == Rational(22, 0) &&
+        Rational(22, 9) + Rational(22, 9) == Rational(44, 9) &&
+        Rational(0, 9) + Rational(22, 9) == Rational(22, 9) &&
+        Rational(-22, 9) + Rational(22, 9) == Rational(0, 9) &&
+        Rational(-22, 0) + Rational(22, 9) == Rational(-22, 0) &&
+
+        Rational(22, 0) + Rational(0, 1) == Rational(22, 0) &&
+        Rational(22, 9) + Rational(0, 1) == Rational(22, 9) &&
+        Rational(0, 9) + Rational(0, 1) == Rational(0, 9) &&
+        Rational(-22, 9) + Rational(0, 1) == Rational(-22, 9) &&
+        Rational(-22, 0) + Rational(0, 1) == Rational(-22, 0) &&
+
+        Rational(22, 0) + Rational(-22, 9) == Rational(22, 0) &&
+        Rational(22, 9) + Rational(-22, 9) == Rational(0, 9) &&
+        Rational(0, 9) + Rational(-22, 9) == Rational(-22, 9) &&
+        Rational(-22, 9) + Rational(-22, 9) == Rational(-44, 9) &&
+        Rational(-22, 0) + Rational(-22, 9) == Rational(-22, 0) &&
+
+        (Rational(22, 0) + Rational(-22, 0)).isNaN() &&
+        Rational(22, 9) + Rational(-22, 0) == Rational(-22, 0) &&
+        Rational(0, 9) + Rational(-22, 0) == Rational(-22, 0) &&
+        Rational(-22, 9) + Rational(-22, 0) == Rational(-22, 0) &&
+        Rational(-22, 0) + Rational(-22, 0) == Rational(-22, 0) &&
+
+        (Rational(22, 0) + Rational(0, 0)).isNaN() &&
+        (Rational(22, 9) + Rational(0, 0)).isNaN() &&
+        (Rational(0, 9) + Rational(0, 0)).isNaN() &&
+        (Rational(-22, 9) + Rational(0, 0)).isNaN() &&
+        (Rational(-22, 0) + Rational(0, 0)).isNaN()
+       ) std::cout << "Summation test passed\n";
+    else std::cout << "Summation test failed\n";
+
+    if ((Rational(22, 0) - Rational(22, 0)).isNaN() &&
+        Rational(22, 9) - Rational(22, 0) == Rational(-22, 0) &&
+        Rational(0, 9) - Rational(22, 0) == Rational(-22, 0) &&
+        Rational(-22, 9) - Rational(22, 0) == Rational(-22, 0) &&
+        Rational(-22, 0) - Rational(22, 0) == Rational(-22, 0) &&
+
+        Rational(22, 0) - Rational(22, 9) == Rational(22, 0) &&
+        Rational(22, 9) - Rational(22, 9) == Rational(0, 9) &&
+        Rational(0, 9) - Rational(22, 9) == Rational(-22, 9) &&
+        Rational(-22, 9) - Rational(22, 9) == Rational(-44, 9) &&
+        Rational(-22, 0) - Rational(22, 9) == Rational(-22, 0) &&
+
+        Rational(22, 0) - Rational(0, 1) == Rational(22, 0) &&
+        Rational(22, 9) - Rational(0, 1) == Rational(22, 9) &&
+        Rational(0, 9) - Rational(0, 1) == Rational(0, 9) &&
+        Rational(-22, 9) - Rational(0, 1) == Rational(-22, 9) &&
+        Rational(-22, 0) - Rational(0, 1) == Rational(-22, 0) &&
+
+        Rational(22, 0) - Rational(-22, 9) == Rational(22, 0) &&
+        Rational(22, 9) - Rational(-22, 9) == Rational(44, 9) &&
+        Rational(0, 9) - Rational(-22, 9) == Rational(22, 9) &&
+        Rational(-22, 9) - Rational(-22, 9) == Rational(0, 9) &&
+        Rational(-22, 0) - Rational(-22, 9) == Rational(-22, 0) &&
+
+        Rational(22, 0) - Rational(-22, 0) == Rational(22, 0) &&
+        Rational(22, 9) - Rational(-22, 0) == Rational(22, 0) &&
+        Rational(0, 9) - Rational(-22, 0) == Rational(22, 0) &&
+        Rational(-22, 9) - Rational(-22, 0) == Rational(22, 0) &&
+        (Rational(-22, 0) - Rational(-22, 0)).isNaN() &&
+
+        (Rational(22, 0) - Rational(0, 0)).isNaN() &&
+        (Rational(22, 9) - Rational(0, 0)).isNaN() &&
+        (Rational(0, 9) - Rational(0, 0)).isNaN() &&
+        (Rational(-22, 9) - Rational(0, 0)).isNaN() &&
+        (Rational(-22, 0) - Rational(0, 0)).isNaN()
+        ) std::cout << "Subtraction test passed\n";
+    else std::cout << "Subtraction test failed\n";
+
+    if (Rational(22, 0) * Rational(22, 0) == Rational(22, 0) &&
+        Rational(22, 9) * Rational(22, 0) == Rational(22, 0) &&
+        (Rational(0, 9) * Rational(22, 0)).isNaN() &&
+        Rational(-22, 9) * Rational(22, 0) == Rational(-22, 0) &&
+        Rational(-22, 0) * Rational(22, 0) == Rational(-22, 0) &&
+
+        Rational(22, 0) * Rational(22, 9) == Rational(22, 0) &&
+        Rational(22, 9) * Rational(22, 9) == Rational(22*22, 9*9) &&
+        Rational(0, 9) * Rational(22, 9) == Rational(0, 9) &&
+        Rational(-22, 9) * Rational(22, 9) == Rational(-22*22, 9*9) &&
+        Rational(-22, 0) * Rational(22, 9) == Rational(-22, 0) &&
+
+        (Rational(22, 0) * Rational(0, 1)).isNaN() &&
+        Rational(22, 9) * Rational(0, 1) == Rational(0, 9) &&
+        Rational(0, 9) * Rational(0, 1) == Rational(0, 9) &&
+        Rational(-22, 9) * Rational(0, 1) == Rational(0, 9) &&
+        (Rational(-22, 0) * Rational(0, 1)).isNaN() &&
+
+        Rational(22, 0) * Rational(-22, 9) == Rational(-22, 0) &&
+        Rational(22, 9) * Rational(-22, 9) == Rational(-22*22, 9*9) &&
+        Rational(0, 9) * Rational(-22, 9) == Rational(0, 9) &&
+        Rational(-22, 9) * Rational(-22, 9) == Rational(22*22, 9*9) &&
+        Rational(-22, 0) * Rational(-22, 9) == Rational(22, 0) &&
+
+        Rational(22, 0) * Rational(-22, 0) == Rational(-22, 0) &&
+        Rational(22, 9) * Rational(-22, 0) == Rational(-22, 0) &&
+        (Rational(0, 9) * Rational(-22, 0)).isNaN() &&
+        Rational(-22, 9) * Rational(-22, 0) == Rational(22, 0) &&
+        Rational(-22, 0) * Rational(-22, 0) == Rational(22, 0) &&
+
+        (Rational(22, 0) * Rational(0, 0)).isNaN() &&
+        (Rational(22, 9) * Rational(0, 0)).isNaN() &&
+        (Rational(0, 9) * Rational(0, 0)).isNaN() &&
+        (Rational(-22, 9) * Rational(0, 0)).isNaN() &&
+        (Rational(-22, 0) * Rational(0, 0)).isNaN()
+        ) std::cout << "Multiplication test passed\n";
+    else std::cout << "Multiplication test failed\n";
+
+    if ((Rational(22, 0) / Rational(22, 0)).isNaN() &&
+        Rational(22, 9) / Rational(22, 0) == Rational(0, 9) &&
+        Rational(0, 9) / Rational(22, 0) == Rational(0, 9) &&
+        Rational(-22, 9) / Rational(22, 0) == Rational(0, 9) &&
+        (Rational(-22, 0) / Rational(22, 0)).isNaN() &&
+
+        Rational(22, 0) / Rational(22, 9) == Rational(22, 0) &&
+        Rational(22, 9) / Rational(22, 9) == Rational(9, 9) &&
+        Rational(0, 9) / Rational(22, 9) == Rational(0, 9) &&
+        Rational(-22, 9) / Rational(22, 9) == Rational(-9, 9) &&
+        Rational(-22, 0) / Rational(22, 9) == Rational(-22, 0) &&
+
+        Rational(22, 0) / Rational(0, 1) == Rational(22, 0) &&
+        Rational(22, 9) / Rational(0, 1) == Rational(22, 0) &&
+        (Rational(0, 9) / Rational(0, 1)).isNaN() &&
+        Rational(-22, 9) / Rational(0, 1) == Rational(-22, 0) &&
+        Rational(-22, 0) / Rational(0, 1) == Rational(-22, 0) &&
+
+        Rational(22, 0) / Rational(-22, 9) == Rational(-22, 0) &&
+        Rational(22, 9) / Rational(-22, 9) == Rational(-9, 9) &&
+        Rational(0, 9) / Rational(-22, 9) == Rational(0, 9) &&
+        Rational(-22, 9) / Rational(-22, 9) == Rational(9, 9) &&
+        Rational(-22, 0) / Rational(-22, 9) == Rational(22, 0) &&
+
+        (Rational(22, 0) / Rational(-22, 0)).isNaN() &&
+        Rational(22, 9) / Rational(-22, 0) == Rational(0, 9) &&
+        Rational(0, 9) / Rational(-22, 0) == Rational(0, 9) &&
+        Rational(-22, 9) / Rational(-22, 0) == Rational(0, 9) &&
+        (Rational(-22, 0) / Rational(-22, 0)).isNaN() &&
+
+        (Rational(22, 0) / Rational(0, 0)).isNaN() &&
+        (Rational(22, 9) / Rational(0, 0)).isNaN() &&
+        (Rational(0, 9) / Rational(0, 0)).isNaN() &&
+        (Rational(-22, 9) / Rational(0, 0)).isNaN() &&
+        (Rational(-22, 0) / Rational(0, 0)).isNaN()
+        ) std::cout << "Division test passed\n";
+    else std::cout << "Division test failed\n";
+
+    if (equal(Rational(-22, -9), 22/9.0) &&
+        equal(Rational(-9, -9), 1) &&
+        equal(Rational(-6, -9), 6/9.0) &&
+        equal(Rational(0, -9), 0) &&
+        equal(Rational(6, -9), -6/9.0) &&
+        equal(Rational(9, -9), -1) &&
+        equal(Rational(22, -9), -22/9.0) &&
+        std::isinf((double)Rational(-9, 0)) &&
+        std::isnan((double)Rational(0, 0)) &&
+        std::isinf((double)Rational(9, 0)) &&
+        equal(Rational(-22, 9), -22/9.0) &&
+        equal(Rational(-9, 9), -1) &&
+        equal(Rational(-6, 9), -6/9.0) &&
+        equal(Rational(0, 9), 0) &&
+        equal(Rational(6, 9), 6/9.0) &&
+        equal(Rational(9, 9), 1) &&
+        equal(Rational(22, 9), 22/9.0)) std::cout << "Conversion to double test passed\n";
+    else std::cout << "Conversion to double test failed\n";
+
+    if (Rational(-22, -9) &&
+        Rational(-9, -9) &&
+        Rational(-6, -9) &&
+        !Rational(0, -9) &&
+        Rational(6, -9) &&
+        Rational(9, -9) &&
+        Rational(22, -9) &&
+        Rational(-9, 0) &&
+        Rational(0, 0) &&
+        Rational(9, 0) &&
+        Rational(-22, 9) &&
+        Rational(-9, 9) &&
+        Rational(-6, 9) &&
+        !Rational(0, 9) &&
+        Rational(6, 9) &&
+        Rational(9, 9) &&
+        Rational(22, 9)) std::cout << "Conversion to bool test passed\n";
+    else std::cout << "Conversion to bool test failed\n";
+}
+
+bool equal(double a, double b, double e) {
+    if (-e < a - b && a - b < e) return true;
+    else return false;
+}
